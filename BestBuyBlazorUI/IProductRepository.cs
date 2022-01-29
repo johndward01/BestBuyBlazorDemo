d18272d99c97427e57fac11c563c2c56dc8951a9
johndward01 <johndward01@gmail.com>
2022-01-29 03:14:19 -0600

    update

---
+++ BestBuyBlazorUI/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using BestBuyBlazorUI.Models;
+
+internal interface IProductRepository
+{
+    public IEnumerable<Product> GetAllProducts();
+    public Product GetProduct(int id);
+    public void UpdateProduct(Product product);
+    public void InsertProduct(Product productToInsert);
+    public IEnumerable<Category> GetCategories();
+    public Product AssignCategory();
+    public void DeleteProduct(Product product);
+}
--- BestBuyBlazorUI/Program.cs
+++ BestBuyBlazorUI/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
